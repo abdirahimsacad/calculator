40f8422a46232205499bfe6ebb7d562cefad550c
abdirahimsacad <abdirahiimsacad@gmail.com>
2023-07-28 08:28:50 -0700

    update

--- challenge.js
+++ challenge.js
@@ -1,2 +1,29 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+function calculator (num1 , num2 , operator){
+    if (operator === "+"){
+     return num1 + num2
+    }
+    
+    else if (operator==="-"){
+        return num1 - num2
+    }
+    else if (operator === "*"){
+        return num1 * num2
+    }
+    
+    else if (operator === "/"){
+        return num1 / num2
+    }
+    else {
+        return "invalid operator"
+    }
+    }
+     console.log(calculator(4,6 , "+"))
+     console.log(calculator(4,6 , "-"))
+     console.log(calculator(4,6 , "*"))
+     console.log(calculator(4,6 , "/"))
+     console.log(calculator(4,6 ,  ""))
+
+     
